578d7a613463cb69e7f3e72cda630375dad7649a
kevinquindi <164789310+kevinquindi@users.noreply.github.com>
2024-10-10 19:30:27 -0300

    Se agrega el import a la bd

--- lib/screens/pedir_turno_screen.dart
+++ lib/screens/pedir_turno_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-//import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:app/entities/servicio.dart';
 import 'package:app/entities/turno.dart';
 import 'package:app/entities/usuario.dart';

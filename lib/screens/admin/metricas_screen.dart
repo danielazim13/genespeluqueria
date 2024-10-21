29d9e67a462f791ff0313c3211ffd8cdc77b8c8d
Sobrino Julian <sobrinojulian@protonmail.com>
2024-10-21 17:31:42 -0300

    /admin/metricas: algo es algo

--- lib/screens/admin/metricas_screen.dart
+++ lib/screens/admin/metricas_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:intl/intl.dart';
 
 class MetricasScreen extends StatelessWidget {
   const MetricasScreen({super.key});
@@ -14,28 +13,16 @@ class MetricasScreen extends StatelessWidget {
 
       // Mapear IDs de servicios a nombres
       Map<String, String> idToName = {};
-      List<String> serviciosDisponibles = [];
       for (var doc in serviciosSnapshot.docs) {
         idToName[doc.id] = doc['nombre'];
-        serviciosDisponibles.add(doc['nombre']);
       }
 
-      // Mapear campos de turno y filtrar por estados permitidos
-      List<Map<String, dynamic>> turnosMapeados =
-          turnosSnapshot.docs.where((turnoDoc) {
-        final turnoData = turnoDoc.data() as Map<String, dynamic>;
-        final estado = turnoData['estado'];
-
-        // Filtrar por los estados permitidos
-        return estado == 'Confirmado' ||
-            estado == 'En proceso' ||
-            estado == 'Realizado';
-      }).map((turnoDoc) {
+      // Mapear turnos
+      List<Map<String, dynamic>> turnosMapeados = turnosSnapshot.docs.map((turnoDoc) {
         final turnoData = turnoDoc.data() as Map<String, dynamic>;
 
-        print(turnoData);
         List<String> nombresServicios = [];
-        for (var idServicio in (turnoData['services'] as List)) {
+        for (var idServicio in (turnoData['servicios'] as List)) {
           if (idToName.containsKey(idServicio)) {
             nombresServicios.add(idToName[idServicio]!);
           }
@@ -46,22 +33,18 @@ class MetricasScreen extends StatelessWidget {
           'ingreso': turnoData.containsKey('ingreso')
               ? (turnoData['ingreso'] as Timestamp).toDate()
               : null,
-          'egreso': turnoData.containsKey('egreso')
-              ? (turnoData['egreso'] as Timestamp).toDate()
-              : null,
-          'services': nombresServicios,
-          'mensaje': turnoData['mensaje'] ?? '',
           'estado': turnoData['estado'] ?? '',
           'precio': (turnoData['precio'] ?? 0).toDouble(),
+          'services': nombresServicios,
         };
       }).toList();
 
+      print(turnosMapeados);
       return {
         'turnos': turnosMapeados,
-        'servicios': serviciosDisponibles,
       };
     } catch (e) {
-      // Manejo de errores
+      // Error handling
       print('Error al obtener los datos: $e');
       return {};
     }
@@ -86,264 +69,51 @@ class MetricasScreen extends StatelessWidget {
             return const Center(child: Text('No hay datos disponibles'));
           }
 
-          return FiltrosYMetrica(
-            turnos: snapshot.data!['turnos'],
-            servicios: snapshot.data!['servicios'],
+          List<Map<String, dynamic>> turnos = snapshot.data!['turnos'];
+
+          return ListView(
+            padding: const EdgeInsets.all(16.0),
+            children: [
+              _buildSection(
+                icon: Icons.event,
+                title: 'Turnos',
+                metrics: [
+                  {'label': 'Total de Turnos', 'value': turnos.length.toString()},
+                  {
+                    'label': 'Turnos Confirmados',
+                    'value': turnos.where((t) => t['estado'] == 'Confirmado').length.toString()
+                  },
+                  {
+                    'label': 'Turnos Realizados',
+                    'value': turnos.where((t) => t['estado'] == 'Realizado').length.toString()
+                  },
+                ],
+              ),
+              const SizedBox(height: 20),
+              const Divider(),
+              const SizedBox(height: 20),
+              _buildSection(
+                icon: Icons.attach_money,
+                title: 'Ingresos',
+                metrics: [
+                  {
+                    'label': 'Total de Ingresos',
+                    'value': '\$${turnos.fold(0.0, (sum, turno) => sum + turno['precio']).toStringAsFixed(2)}'
+                  },
+                ],
+              ),
+            ],
           );
         },
       ),
     );
   }
-}
 
-class FiltrosYMetrica extends StatefulWidget {
-  final List<Map<String, dynamic>> turnos;
-  final List<String> servicios;
-
-  const FiltrosYMetrica({
-    super.key,
-    required this.turnos,
-    required this.servicios,
-  });
-
-  @override
-  _FiltrosYMetricaState createState() => _FiltrosYMetricaState();
-}
-
-class _FiltrosYMetricaState extends State<FiltrosYMetrica> {
-  DateTime? _fechaInicio;
-  DateTime? _fechaFin;
-  String? _selectedTipoServicio;
-  List<Map<String, dynamic>> turnosFiltrados = [];
-
-  @override
-  void initState() {
-    super.initState();
-    turnosFiltrados = widget.turnos;
-  }
-
-  Future<void> _seleccionarFechaInicio(BuildContext context) async {
-    final picked = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2101),
-    );
-    if (picked != null) {
-      setState(() {
-        _fechaInicio = picked;
-        _filtrarTurnos();
-      });
-    }
-  }
-
-  Future<void> _seleccionarFechaFin(BuildContext context) async {
-    final picked = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2101),
-    );
-    if (picked != null) {
-      setState(() {
-        _fechaFin = picked;
-        _filtrarTurnos();
-      });
-    }
-  }
-
-  void _filtrarTurnos() {
-    setState(() {
-      turnosFiltrados = widget.turnos.where((turno) {
-        final ingreso = turno['ingreso'] as DateTime?;
-        final egreso = turno['egreso'] as DateTime?;
-        bool cumpleFecha = true;
-        bool cumpleServicio = true;
-
-        if (_fechaInicio != null && _fechaFin != null) {
-          cumpleFecha = (ingreso != null &&
-                  ingreso.isAfter(_fechaInicio!) &&
-                  ingreso.isBefore(_fechaFin!)) ||
-              (egreso != null &&
-                  egreso.isAfter(_fechaInicio!) &&
-                  egreso.isBefore(_fechaFin!));
-        }
-
-        if (_selectedTipoServicio != null) {
-          cumpleServicio = turno['services'].contains(_selectedTipoServicio);
-        }
-
-        return cumpleFecha && cumpleServicio;
-      }).toList();
-    });
-  }
-
-  void _borrarFiltros() {
-    setState(() {
-      _fechaInicio = null;
-      _fechaFin = null;
-      _selectedTipoServicio = null;
-      turnosFiltrados = widget.turnos;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final totalTurnos = turnosFiltrados.length;
-    final turnosConfirmados =
-        turnosFiltrados.where((t) => t['state'] == 'Confirmado').length;
-    final turnosRealizados =
-        turnosFiltrados.where((t) => t['state'] == 'Realizado').length;
-    final totalServicios =
-        turnosFiltrados.expand((t) => t['services'] as List<String>).length;
-    final totalIngresos = turnosFiltrados.fold(
-        0.0, (sum, turno) => sum + (turno['precio'] as double));
-    final promedioIngresosPorServicio =
-        totalServicios > 0 ? totalIngresos / totalServicios : 0.0;
-
-    // Calcular tiempo promedio por servicio (en días)
-    double tiempoPromedioPorServicio = 0.0;
-    int cantidadTurnosRealizados = 0;
-
-    for (var turno in turnosFiltrados) {
-      if (turno['state'] == 'Realizado' &&
-          turno['ingreso'] != null &&
-          turno['egreso'] != null) {
-        final DateTime ingreso = turno['ingreso'];
-        final DateTime egreso = turno['egreso'];
-        final int dias = egreso.difference(ingreso).inDays;
-        tiempoPromedioPorServicio += dias;
-        cantidadTurnosRealizados++;
-      }
-    }
-
-    tiempoPromedioPorServicio = cantidadTurnosRealizados > 0
-        ? tiempoPromedioPorServicio / cantidadTurnosRealizados
-        : 0.0;
-
-    return ListView(
-      padding: const EdgeInsets.all(16.0),
-      children: [
-        ExpansionTile(
-          title: const Text('Filtros'),
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () => _seleccionarFechaInicio(context),
-                    child: Text(_fechaInicio == null
-                        ? 'Fecha inicio'
-                        : DateFormat('dd/MM/yyyy').format(_fechaInicio!)),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () => _seleccionarFechaFin(context),
-                    child: Text(_fechaFin == null
-                        ? 'Fecha fin'
-                        : DateFormat('dd/MM/yyyy').format(_fechaFin!)),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: DropdownButton<String>(
-                      isExpanded: true,
-                      value: _selectedTipoServicio,
-                      hint: const Text('Tipo de Servicio'),
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          _selectedTipoServicio = newValue;
-                          _filtrarTurnos();
-                        });
-                      },
-                      items: widget.servicios
-                          .map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                    ),
-                  ),
-                  const SizedBox(width: 10),
-                  ElevatedButton(
-                    onPressed: _borrarFiltros,
-                    child: const Text('Borrar filtros'),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 15),
-          ],
-        ),
-        const SizedBox(height: 20),
-        const Divider(),
-        const SizedBox(height: 20),
-        _buildSection(
-          icon: Icons.event,
-          title: 'Turnos',
-          metrics: [
-            {'label': 'Total de Turnos', 'value': totalTurnos.toString()},
-            {
-              'label': 'Turnos Confirmados',
-              'value': turnosConfirmados.toString()
-            },
-            {
-              'label': 'Turnos Realizados',
-              'value': turnosRealizados.toString()
-            },
-          ],
-        ),
-        const SizedBox(height: 20),
-        const Divider(),
-        const SizedBox(height: 20),
-        _buildSection(
-          icon: Icons.build,
-          title: 'Servicios',
-          metrics: [
-            {'label': 'Total de Servicios', 'value': totalServicios.toString()},
-            {
-              'label': 'Tiempo Promedio por Servicio (días)',
-              'value': tiempoPromedioPorServicio.toStringAsFixed(2)
-            },
-          ],
-        ),
-        const SizedBox(height: 20),
-        const Divider(),
-        const SizedBox(height: 20),
-        _buildSection(
-          icon: Icons.attach_money,
-          title: 'Ingresos',
-          metrics: [
-            {
-              'label': 'Total de Ingresos',
-              'value': '\$${totalIngresos.toStringAsFixed(2)}'
-            },
-            {
-              'label': 'Promedio Ingreso por Servicio',
-              'value': '\$${promedioIngresosPorServicio.toStringAsFixed(2)}'
-            },
-          ],
-        ),
-      ],
-    );
-  }
-
-  Widget _buildSection(
-      {required IconData icon,
-      required String title,
-      required List<Map<String, String>> metrics}) {
+  Widget _buildSection({
+    required IconData icon,
+    required String title,
+    required List<Map<String, String>> metrics,
+  }) {
     return Card(
       child: Column(
         children: [
@@ -351,8 +121,7 @@ class _FiltrosYMetricaState extends State<FiltrosYMetrica> {
             leading: Icon(icon),
             title: Text(
               title,
-              style:
-                  const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              style: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
             ),
           ),
           ...metrics.map(
@@ -360,8 +129,7 @@ class _FiltrosYMetricaState extends State<FiltrosYMetrica> {
               title: Text(metric['label']!),
               trailing: Text(
                 metric['value']!,
-                style: const TextStyle(
-                    fontSize: 16.0, fontWeight: FontWeight.bold),
+                style: const TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
               ),
             ),
           ),

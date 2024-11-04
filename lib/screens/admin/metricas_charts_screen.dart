b38b7a557df7bd05c50216021ed0edae261b229c
npena21 <82242407+npena21@users.noreply.github.com>
2024-11-04 17:42:04 -0300

    Update metricas_charts_screen.dart

--- lib/screens/admin/metricas_charts_screen.dart
+++ lib/screens/admin/metricas_charts_screen.dart
@@ -13,12 +13,28 @@ class MetricasChartsScreen extends StatelessWidget {
           await FirebaseFirestore.instance.collection('turns').get();
       List<DChartBarDataCustom> data = [];
 
-      for (var doc in snapshot.docs) {
-        data.add(DChartBarDataCustom(
-          value: (doc['precio'] as num).toDouble(), // Conversión a double
-          label: doc['estado'],
-        ));
-      }
+      List<String> estados = ['Pendiente','Realizado','Confirmado'];
+            List<int> estadosCont = [0,0,0];
+             
+
+            for (var doc in snapshot.docs) {
+
+               if ( estados[0].contains(doc['estado'])){
+                  estadosCont[0]++;
+               } else if ( estados[1].contains(doc['estado'])){
+                  estadosCont[1]++;
+               } else if ( estados[2].contains(doc['estado'])){
+                  estadosCont[2]++;
+               }           
+            }
+
+          for (int i = 0; i < estados.length; i++) {
+               data.add(DChartBarDataCustom(
+                value: estadosCont[i].toDouble(), 
+                label: estados[i],    
+              ));
+            }
+
 
       return data;
     }
@@ -58,167 +74,4 @@ class MetricasChartsScreen extends StatelessWidget {
       ),
     );
   }
-}
-
-class MyBarChart extends StatelessWidget {
-  const MyBarChart({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return BarChart(
-      BarChartData(
-        barTouchData: barTouchData,
-        titlesData: titlesData,
-        borderData: borderData,
-        barGroups: barGroups,
-        gridData: FlGridData(show: true),
-        alignment: BarChartAlignment.spaceAround,
-        maxY: 20,
-      ),
-    );
-  }
-
-  BarTouchData get barTouchData => BarTouchData(
-        enabled: false,
-        touchTooltipData: BarTouchTooltipData(
-          tooltipPadding: EdgeInsets.zero,
-          tooltipMargin: 8,
-          getTooltipItem: (
-            BarChartGroupData group,
-            int groupIndex,
-            BarChartRodData rod,
-            int rodIndex,
-          ) {
-            return BarTooltipItem(
-              rod.toY.toString(),
-              const TextStyle(color: Colors.cyan, fontWeight: FontWeight.bold),
-            );
-          },
-        ),
-      );
-
-  Widget getTitles(double value, TitleMeta meta) {
-    final style = TextStyle(
-      color: Colors.blue,
-      fontWeight: FontWeight.bold,
-      fontSize: 10,
-    );
-    String text;
-    switch (value.toInt()) {
-      case 0:
-        text = 'Mn';
-        break;
-      case 1:
-        text = 'Te';
-        break;
-      case 2:
-        text = 'Wd';
-        break;
-      case 3:
-        text = 'Tu';
-        break;
-      case 4:
-        text = 'Fr';
-        break;
-      case 5:
-        text = 'St';
-        break;
-      case 6:
-        text = 'Sn';
-        break;
-      default:
-        text = '..';
-        break;
-    }
-    return SideTitleWidget(
-      axisSide: meta.axisSide,
-      space: 2,
-      child: Text(text, style: style),
-    );
-  }
-
-  FlTitlesData get titlesData => FlTitlesData(
-        show: true,
-        bottomTitles: AxisTitles(
-          sideTitles: SideTitles(
-            showTitles: true,
-            reservedSize: 30,
-            getTitlesWidget: getTitles,
-          ),
-        ),
-        leftTitles: AxisTitles(
-          sideTitles: SideTitles(showTitles: true),
-        ),
-        topTitles: AxisTitles(
-          sideTitles: SideTitles(showTitles: false),
-        ),
-        rightTitles: AxisTitles(
-          sideTitles: SideTitles(showTitles: false),
-        ),
-      );
-
-  FlBorderData get borderData => FlBorderData(
-        show: false,
-      );
-
-  LinearGradient get _barsGradient => LinearGradient(
-        colors: [
-          Colors.blueAccent,
-          Colors.redAccent,
-        ],
-        begin: Alignment.bottomCenter,
-        end: Alignment.bottomCenter,
-      );
-
-  List<BarChartGroupData> get barGroups => [
-        BarChartGroupData(
-          x: 0,
-          barRods: [
-            BarChartRodData(toY: 8, gradient: _barsGradient),
-          ],
-          showingTooltipIndicators: [0],
-        ),
-        BarChartGroupData(
-          x: 1,
-          barRods: [
-            BarChartRodData(toY: 10, gradient: _barsGradient),
-          ],
-          showingTooltipIndicators: [0],
-        ),
-        BarChartGroupData(
-          x: 2,
-          barRods: [
-            BarChartRodData(toY: 15, gradient: _barsGradient),
-          ],
-          showingTooltipIndicators: [0],
-        ),
-        BarChartGroupData(
-          x: 3,
-          barRods: [
-            BarChartRodData(toY: 18, gradient: _barsGradient),
-          ],
-          showingTooltipIndicators: [0],
-        ),
-        BarChartGroupData(
-          x: 4,
-          barRods: [
-            BarChartRodData(toY: 5, gradient: _barsGradient),
-          ],
-          showingTooltipIndicators: [0],
-        ),
-        BarChartGroupData(
-          x: 5,
-          barRods: [
-            BarChartRodData(toY: 9, gradient: _barsGradient),
-          ],
-          showingTooltipIndicators: [0],
-        ),
-        BarChartGroupData(
-          x: 6,
-          barRods: [
-            BarChartRodData(toY: 5, gradient: _barsGradient),
-          ],
-          showingTooltipIndicators: [0],
-        ),
-      ];
 }
